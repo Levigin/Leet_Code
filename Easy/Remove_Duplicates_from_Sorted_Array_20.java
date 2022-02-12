470de4f14a36c54dd214be8911e672066346faaf
Evgeniy <serganates@gmail.com>
2022-02-12 10:20:12 +0300

    Creat a new tasck in cataloc Easy

---
+++ Easy/Remove_Duplicates_from_Sorted_Array_20.java
@@ -0,0 +1,14 @@
+package Leet_Code.Easy;
+
+public class Remove_Duplicates_from_Sorted_Array_20 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int removeDuplicates(int[] nums) {
+
+        return 123;
+
+    }
+}
