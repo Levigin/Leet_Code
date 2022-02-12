dc4be9606836c0d09f661fb3d3e7d26da2b65046
Evgeniy <serganates@gmail.com>
2022-02-12 20:51:11 +0300

    solved the problem 20.

--- Easy/Remove_Duplicates_from_Sorted_Array_20.java
+++ Easy/Remove_Duplicates_from_Sorted_Array_20.java
@@ -1,14 +1,35 @@
 package Leet_Code.Easy;
 
+import java.util.Arrays;
+
 public class Remove_Duplicates_from_Sorted_Array_20 {
     public static void main(String[] args) {
 
+        int[] nums = {1, 1,2,3,4,4,4,4,5,5,98};
+
+
+
+        System.out.println(removeDuplicates(nums)); //{1,2,3,4,5,6,_,_,_}
+
+        System.out.println(Arrays.toString(nums));
+
 
     }
 
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
 
-        return 123;
+        int counter = 1;
+
+
+
+        for (int i = 0; i < nums.length-1; i++)
+            if (nums[i] != nums[i+1]){
+                nums[counter++] = nums[i+1];
+
+            }
+        return counter;
 
     }
+
 }
+
