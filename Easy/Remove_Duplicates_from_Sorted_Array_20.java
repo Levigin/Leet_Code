c9f70618114da1d12a038b83ec1021fd89ece6a9
Evgeniy <serganates@gmail.com>
2022-02-12 22:06:26 +0300

    delete unnecessary lines 20.

--- Easy/Remove_Duplicates_from_Sorted_Array_20.java
+++ Easy/Remove_Duplicates_from_Sorted_Array_20.java
@@ -7,20 +7,15 @@ public class Remove_Duplicates_from_Sorted_Array_20 {
 
         int[] nums = {1, 1,2,3,4,4,4,4,5,5,98};
 
-
-
         System.out.println(removeDuplicates(nums)); //{1,2,3,4,5,6,_,_,_}
 
         System.out.println(Arrays.toString(nums));
-
 
     }
 
     public static int removeDuplicates(int[] nums) {
 
         int counter = 1;
-
-
 
         for (int i = 0; i < nums.length-1; i++)
             if (nums[i] != nums[i+1]){
